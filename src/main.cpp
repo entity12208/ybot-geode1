e8669470d8ba010431fc57622e5c96e805b8856d
entity12208 <116198380+entity12208@users.noreply.github.com>
2025-06-10 15:03:10 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,60 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/ui/GeodeUI.hpp>
+#include "YBot.hpp"
+#include <fstream>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+static bool g_botEnabled = false;
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+namespace YBot {
+    void toggleBotMode(bool enabled) {
+        g_botEnabled = enabled;
+    }
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+    bool isBotEnabled() {
+        return g_botEnabled;
+    }
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+    void logDeath(float x, float y) {
+        std::ofstream logFile(Mod::get()->getSaveDir() / "YBot.log", std::ios::app);
+        if (logFile.is_open()) {
+            logFile << "Death at X: " << x << ", Y: " << y << std::endl;
+            logFile.close();
+        }
+    }
+}
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+class $modify(YBotPlayerObject, PlayerObject) {
+    void destroyPlayer() {
+        if (YBot::isBotEnabled()) {
+            auto pos = this->getPosition();
+            log::info("YBot - Died at X: {}, Y: {}", pos.x, pos.y);
+            YBot::logDeath(pos.x, pos.y);
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+            if (auto playLayer = GameManager::sharedState()->getPlayLayer()) {
+                playLayer->resetLevel();
+                return;
+            }
+        }
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
-
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+        PlayerObject::destroyPlayer();
+    }
 };
+
+$on_mod(Loaded) {
+    Mod::get()->addCustomUI([] {
+        auto menu = MenuLayer::create();
+        auto toggle = CCMenuItemToggler::createWithStandardSprites(
+            [](CCObject*) {
+                YBot::toggleBotMode(!YBot::isBotEnabled());
+                FLAlertLayer::create("YBot", YBot::isBotEnabled() ? "Bot Mode Enabled" : "Bot Mode Disabled", "OK")->show();
+            },
+            [] { return false; }
+        );
+        toggle->setPosition(100, 100);
+        menu->addChild(toggle);
+        return menu;
+    });
+}
